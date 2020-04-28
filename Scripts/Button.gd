d0872597dfb750f4975694c67a620cbd3a16d8e3
Jenni <justlittleoldmejenn@gmail.com>
2020-04-27 19:48:51 -0700

    Added Start Menu

---
+++ Scripts/Button.gd
@@ -0,0 +1,5 @@
+extends Button
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Assets/Main_Project_Scenes/Main.tscn")
--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -9,6 +9,10 @@ extends Spatial
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+
+
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
